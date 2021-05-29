4d80ef47cfa9361f818aba2c50a7d089cda40b49
Yunus Emre <71335852+Ynsemretkgz@users.noreply.github.com>
2021-05-29 18:02:28 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+#Config file
+//Site Title
+$title = "Emre";
+
+//Site URL
+$site_url = "#";
+
+//Site Description
+$description = "";
+
+//Watermark image
+$watermarkImage = "";
+
+//Watermark link
+$watermarkLink = "";
+
+//Google Analytics tag ID
+$gaCode = "UA-69771681-1";
+
+//Preroll Ads Video (.mp4)
+$prerollAds = "https://raw.githubusercontent.com/telase/PRO/master/vids/flash.mp4";
+
+//Preroll Ads Time (seconds)
+$prerollTime = 3;
+
+//Preroll Ads External Link
+$prerollLink = "";
+?>
